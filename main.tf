5146bd1b1a046e1d92eee75be56dc9a31a030ce4
Narayan Parvatikar <narayan.parvatikar@nvisionx.ai>
2026-02-18 00:35:41 +0530

    Remove provider blocks and require caller to pass provider aliases

--- main.tf
+++ main.tf
@@ -3,6 +3,25 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.0"
+      configuration_aliases = [
+        aws.us_east_1,
+        aws.us_east_2,
+        aws.us_west_1,
+        aws.us_west_2,
+        aws.eu_west_1,
+        aws.eu_west_2,
+        aws.eu_west_3,
+        aws.eu_central_1,
+        aws.eu_north_1,
+        aws.ap_south_1,
+        aws.ap_northeast_1,
+        aws.ap_northeast_2,
+        aws.ap_northeast_3,
+        aws.ap_southeast_1,
+        aws.ap_southeast_2,
+        aws.ca_central_1,
+        aws.sa_east_1,
+      ]
     }
   }
 }
@@ -407,112 +426,3 @@ module "inventory_collector_sa_east_1" {
   encryption_algorithm               = var.encryption_algorithm
   tags                               = var.tags
 }
-
-
-# =============================================================================
-# Provider Aliases - US Regions
-# =============================================================================
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "us_east_2"
-  region = "us-east-2"
-}
-
-provider "aws" {
-  alias  = "us_west_1"
-  region = "us-west-1"
-}
-
-provider "aws" {
-  alias  = "us_west_2"
-  region = "us-west-2"
-}
-
-# =============================================================================
-# Provider Aliases - EU Regions
-# =============================================================================
-
-provider "aws" {
-  alias  = "eu_west_1"
-  region = "eu-west-1"
-}
-
-provider "aws" {
-  alias  = "eu_west_2"
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "eu_west_3"
-  region = "eu-west-3"
-}
-
-provider "aws" {
-  alias  = "eu_central_1"
-  region = "eu-central-1"
-}
-
-provider "aws" {
-  alias  = "eu_north_1"
-  region = "eu-north-1"
-}
-
-
-# =============================================================================
-# Provider Aliases - Asia Pacific Regions
-# =============================================================================
-
-provider "aws" {
-  alias  = "ap_south_1"
-  region = "ap-south-1"
-}
-
-provider "aws" {
-  alias  = "ap_northeast_1"
-  region = "ap-northeast-1"
-}
-
-provider "aws" {
-  alias  = "ap_northeast_2"
-  region = "ap-northeast-2"
-}
-
-provider "aws" {
-  alias  = "ap_northeast_3"
-  region = "ap-northeast-3"
-}
-
-provider "aws" {
-  alias  = "ap_southeast_1"
-  region = "ap-southeast-1"
-}
-
-provider "aws" {
-  alias  = "ap_southeast_2"
-  region = "ap-southeast-2"
-}
-
-# =============================================================================
-# Provider Aliases - Canada Regions
-# =============================================================================
-
-provider "aws" {
-  alias  = "ca_central_1"
-  region = "ca-central-1"
-}
-
-# =============================================================================
-# Provider Aliases - South America Regions
-# =============================================================================
-
-provider "aws" {
-  alias  = "sa_east_1"
-  region = "sa-east-1"
-}
-
-
